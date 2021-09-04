308768d0d077b2a9fde4378984086ae4b0765e3c
Kelvin Lee <kelvinlee@live.co.uk>
2021-09-04 18:05:54 +0100

    Add ship row and column location and set to the board

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ board = []
 for x in range(7):
     board.append(["-"] * 7)
 
+
 def print_board(board):
     """
     Prints the correct format for CPU the board
@@ -13,3 +14,22 @@ def print_board(board):
         print(" ".join(row))
 
 print_board(board)
+
+
+def location_col(board):
+    """
+    Generate a random column for the CPU ship.
+    """
+    return randint(0, len(board) - 1)
+
+def location_row(board):
+    """
+    Generate a random row for the CPU ship.
+    """
+    return randint(0, len(board) - 1)
+
+ship_row = location_row(board)
+ship_col = location_col(board)
+
+print(ship_col)
+print(ship_row)
